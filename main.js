8dd5bf3701a1ad76d470eb4d92a00cdaab7a53b4
Nikunj-Bhardwaj <88379545+Nikunj-Bhardwaj@users.noreply.github.com>
2021-12-24 15:34:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
-noseX=0;
-noseY=0;
+mustache_x=0;
+mustache_y=0;
 
-function preload() {
-  clown_nose = loadImage('https://i.postimg.cc/vmmssS1r/m.png');
+function preload(){
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
-function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.size(300, 300);
-  video.hide();
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
 
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPose);
 }
 
-function modelLoaded() {
-  console.log('PoseNet Is Initialized');
+function draw(){
+    image(video,0,0,300,300);
+    image(mustache,mustache_x,mustache_y,60,45);
 }
 
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x-20;
-    noseY = results[0].pose.nose.y-15;
-  }
+function take_snapshot(){
+    save("my_picture.png");
 }
 
-function draw() {
-  image(video, 0, 0, 300, 300);
-  image(clown_nose, noseX, noseY, 30, 30);
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
 }
 
-function take_snapshot(){    
-  save('rockstar.png');
+function gotPose(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x: "+results[0].pose.nose.x);
+        mustache_x = results[0].pose.nose.x-30;
+        console.log("nose y: "+results[0].pose.nose.y);
+        mustache_y = results[0].pose.nose.y;
+    };
 }
